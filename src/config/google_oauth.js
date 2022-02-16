c3d5c1916b98b09a23e11ba10eed1b0945e4db7c
yashsakalley-1997 <sakalley.yash@gmail.com>
2022-02-17 00:46:33 +0530

    changed callback url in google_auth.js

--- src/config/google_oauth.js
+++ src/config/google_oauth.js
@@ -8,7 +8,7 @@ const User = require("../models/user.model");
 passport.use(new GoogleStrategy({
     clientID:     process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:5000/auth/google/callback",
+    callbackURL: "http://34.205.157.165:5000/auth/google/callback",
     passReqToCallback   : true
   },
   async function(request, accessToken, refreshToken, profile, done) {
